a69020fee98ccecadffaff5269a1a8117e0b6b4d
supvato <gabelee75@yahoo.com>
2020-04-07 19:22:54 -0700

    Created new commands (forcejoin, forceleave, leave, captains

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import discord
 import asyncio
 import random
-import botToken
 from discord.ext import commands
 
 description = '''An example bot to showcase the discord.ext.commands extension
@@ -16,7 +15,9 @@ disc = discord.Client
 on = False
 f = 0
 inHouseStatus = 0
+poolOpenStatus = False
 inHousePool = []
+inHouseQueue =[]
 inHouseTeam1 = []
 inHouseTeam2 = []
 uwuFile = open("uwuCount.txt", "r")
@@ -25,11 +26,13 @@ for x in uwuFile:
     str1 = x.split(', ')
     uwuDict[str(str1[0])] = str(str1[1])
 
-
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
 
+# @bot.event
+# async def on_typing(channel,msg, when):
+#     await channel.send('Stop typing')
 
 @bot.event
 async def on_message(message):
@@ -58,7 +61,6 @@ async def on_message(message):
         await message.channel.send(message.content)
     await bot.process_commands(message)
 
-
 @bot.command()
 async def clear(ctx, amount: int):
     await ctx.channel.purge(limit=1)
@@ -70,12 +72,10 @@ async def clear(ctx, amount: int):
     await ctx.channel.purge(limit=amount)
     await ctx.send('Done!', delete_after=5)
 
-
 @bot.command()
 async def commands(ctx):
     await ctx.channel.purge(limit=1)
     await ctx.send('```ur gay```', delete_after=30)
-
 
 @bot.command()
 async def p(ctx, name: str, amount=3.0):
@@ -100,7 +100,6 @@ async def pp(ctx):
     await ctx.channel.purge(limit=1)
     global on
     on = not on
-
 
 @bot.command()
 async def summon(ctx):
@@ -127,7 +126,6 @@ async def move(ctx, amount: int, userId=221417504944160770):
         print(count)
         await ctx.guild.get_member(userId).edit(voice_channel=voicechannels[count])
         await asyncio.sleep(1)
-
 
 @bot.command()
 async def stoppls(ctx):
@@ -179,8 +177,10 @@ async def superhere(ctx):
 @bot.command()
 async def inhouse(ctx, status=1):
     global inHouseStatus
+    global poolOpenStatus
     if(inHouseStatus == 0 and status==1):
         await ctx.channel.send('Starting in house session...')
+        poolOpenStatus = True
     elif(status==0):
         await ctx.channel.send('Ending in house session...')
     elif(inHouseStatus == 1 and status != 0):
@@ -189,18 +189,49 @@ async def inhouse(ctx, status=1):
         await ctx.channel.send('No in house in progress...')
     inHouseStatus = status
 
+@bot.command()
+async def forcejoin(ctx):
+    for i in ctx.message.mentions:
+        addPlayerPool(i.id)
+
 
 @bot.command()
 async def join(ctx):
+    global poolOpenStatus
+    userIn = checkIfIn(ctx.message.author.id)
     await ctx.channel.purge(limit=1)
     if (inHouseStatus == 1):
-        if ctx.message.author != bot.user and len(inHousePool) <= 9 and ctx.message.author.id not in inHousePool:
-            await ctx.channel.send('Adding ' + '<@' + str(ctx.message.author.id) + '> to user pool')
-            inHousePool.append(ctx.message.author.id)
-        elif(len(inHousePool) >= 10):
-            await ctx.channel.send('No more players can join')
-        elif(ctx.message.author.id  in inHousePool):
+        if(poolOpenStatus == False):
+            await ctx.channel.send('No players can join at the moment')
+        if(userIn == True):
             await ctx.channel.send('You\'re already in dummy')
+        elif ctx.message.author != bot.user and len(inHousePool) <= 9 and ctx.message.author.id not in inHousePool:
+            await ctx.channel.send('Adding ' + '<@' + str(ctx.message.author.id) + '> to user pool')
+            addPlayerPool(ctx.message.author.id)
+            if(len(inHousePool) == 10):
+                poolOpenStatus = False
+                ctx.channel.send('The user pool is now closed, adding ' + '<@' + str(ctx.message.author.id) + '> to the queue')
+                addPlayerQueue(ctx.message.author.id)
+                await ctx.channel.send('Adding ' + '<@' + str(ctx.message.author.id) + '> to queue')
+    else:
+        await ctx.channel.send('No in house in progress')
+
+@bot.command()
+async def forceleave(ctx):
+    for i in ctx.message.mentions:
+        removePlayerPool(i.id)
+        removePlayerTeam1(i.id)
+        removePlayerTeam2(i.id)
+
+@bot.command()
+async def leave(ctx):
+    await ctx.channel.purge(limit=1)
+    if(inHouseStatus == 1):
+        removePlayerPool(ctx.message.author.id)
+        removePlayerTeam1(ctx.message.author.id)
+        removePlayerTeam2(ctx.message.author.id)
+        await ctx.channel.send('Removing ' + '<@' + str(ctx.message.author.id) + '>')
+
     else:
         await ctx.channel.send('No in house in progress')
 
@@ -217,9 +248,35 @@ async def players(ctx):
 
         await ctx.channel.send('Players in team 2: ')
         for i in inHouseTeam2:
-                await ctx.channel.send('<@' + str(i) + '>')
+            await ctx.channel.send('<@' + str(i) + '>')
+
+        await ctx.channel.send('Players in queue:')
+        for i in inHouseQueue:
+            await ctx.channel.send('<@' + str(i) + '>')
     else:
         await ctx.channel.send('No in house in progress')
+
+@bot.command()
+async def captains(ctx):
+    captains = ctx.message.mentions
+
+    if(len(captains) > 2):
+        await ctx.channel.send('You included too many captains, only the first two will be assigned')
+    elif(len(captains) < 2):
+        await ctx.channel.send('You didn\'t include enough captains')
+    elif(captains[0].id in inHousePool and captains[1].id in inHousePool):
+        addPlayerTeam1(captains[0].id)
+        addPlayerTeam2(captains[1].id)
+        removePlayerPool(captains[0].id)
+        removePlayerPool(captains[1].id)
+    else:
+        await ctx.channel.send('One or more of the captains you selected are not part of the pool and therefore were not added')
+
+def checkIfIn(id:int):
+    if(id in inHouseQueue or id in inHousePool or id in inHouseTeam1 or id in inHouseTeam2):
+        return True
+    else:
+        return False
 
 def addPlayerTeam1(id:int):
     inHouseTeam1.append(id)
@@ -227,13 +284,27 @@ def addPlayerTeam1(id:int):
 def addPlayerTeam2(id:int):
     inHouseTeam2.append(id)
 
+def addPlayerPool(id:int):
+    inHousePool.append(id)
+
+def addPlayerQueue(id: int):
+    inHouseQueue.append(id)
 
 def removePlayerTeam1(id: int):
-    inHouseTeam1.pop(id)
-
+    if(id in inHouseTeam1):
+        inHouseTeam1.remove(id)
 
 def removePlayerTeam2(id: int):
-    inHouseTeam2.pop(id)
+    if (id in inHouseTeam2):
+        inHouseTeam2.remove(id)
+
+def removePlayerPool(id: int):
+    if (id in inHousePool):
+        inHousePool.remove(id)
+
+def removePlayerQueue(id: int):
+    if (id in inHouseQueue):
+        inHouseQueue.remove(id)
 
 
 
@@ -245,4 +316,4 @@ def removePlayerTeam2(id: int):
 
 
 
-bot.run(botToken.YOUR_TOKEN)
+bot.run('Njc2MzM0MTA4Nzc4OTU0Nzcx.XowYnQ.PBNyQQ6z_fgwbmTlei7d8174njY')
